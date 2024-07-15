3ef104d78e58d384446cced6f492f08349f14fc4
Julien TANGUY <tanguyjulien@hotmail.fr>
2024-07-15 13:50:14 +0200

    centrer la header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,7 +22,7 @@ export default function Header() {
 
   return (
     <header className="fixed w-full bg-background top-0 right-0 left-0 p-5 z-50">
-      <section className="flex gap-4 justify-between">
+      <section className="flex gap-4 m-auto max-w-5xl">
         <div className="flex-[1] flex gap-2 items-center">
           <Avatar>
             <AvatarImage src="profile-pic2.png" alt="photo-profil" />
